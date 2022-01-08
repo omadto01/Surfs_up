efa527e73ab48cb89c3c93d57d778c3a1b7308b2
omadto01 <omadto.omar@hotmail.com>
2022-01-08 18:13:07 -0500

    Build a Climate App with Flask

--- app.py
+++ app.py
@@ -1,14 +1,113 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# from flask import Flask
+
+#################################
+# Database Setup
+#################################
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+#reflect an exisiting database into a new model
+Base = automap_base()
+
+#reflect the tables
+Base.prepare(engine, reflect=True)
+
+#Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Create our session from python to the DB
+session = Session(engine)
+
+####################################
+# Flask Setup
+####################################
 app = Flask(__name__)
+
+# import app
+
+# print("example __name__ = %s", __name__)
+
+# if __name__ == "__main__":
+#     print("example is being run directly.")
+# else:
+#     print("example is being imported")
 
 @app.route('/')
 
-def hello_world():
-    return 'Hello world'
+# def hello_world():
+#     return 'Hello world'
+#############################
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
+    /api/v1.0/temp/start/end<br/>
+    ''')
 
-# export FLASK_APP=app.py
+@app.route("/api/v1.0/precipitation")
 
-# set FLASK_APP=app.py
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
 
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>") 
+#/api/v1.0/temp/2017-06-01/2017-06-30
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps=temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)   
+
+if __name__ == "__main__":
+    app.run()
+
+# Go to Ananconda prompt 
+# cd desktop > Surfs_up folder > ls
 # flask run
